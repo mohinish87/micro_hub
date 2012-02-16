aadbda7be50f98ad18b0dd42d0c05a3b5d00d8d6
mohinish <mohinish.rao@gmail.com>
2012-02-16 14:01:33 -0500

    Trying to learn ROR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroHub::Application.config.secret_token = '5478917721cf5e134d038ed2ef53061ff9e9e860c10341f73a40e905eedb92da726f21090e2c26ba075d345add9f14da5b3ae56803bf5bf035d83db8e63ea721'
